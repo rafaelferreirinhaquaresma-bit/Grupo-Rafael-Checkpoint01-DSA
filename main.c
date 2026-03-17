837c954154f483b6b8a4b310f9d4194dbe6c221b
Rafael quaresma <rafaelferreirinhaquaresma@gmail.com>
2026-03-17 11:57:33 -0300

    separa a linha do primeiro prit

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int main(){
-    printf("Rafael Ferreirinha Quaresma");
-    printf("Aluno 2");
+    printf("Rafael Ferreirinha Quaresma\n");
+    printf("Aluno 2\n");
     printf("Aluno 3");
     return 0;
 }
